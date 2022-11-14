5566ffa2d211e5001b9166eb75d7c48d4d710952
ryan <ryan@DMaquina2.myguest.virtualbox.org>
2022-11-13 23:40:31 -0300

    save

--- server.py
+++ server.py
@@ -5,6 +5,21 @@ import subprocess
 import time
 import sctp
 
+def run_tcp_dump():
+    response = "\nRESPONSE FROM IP: " + ip + " HOSTNAME: " + socket.gethostname() + "\n\n"
+    outfile = open('out.txt','w')
+    status = subprocess.Popen(['tcpdump', '-n'], bufsize=0, stdout=outfile)
+    time.sleep(10)
+    status.terminate()
+    outfile.close()
+    time.sleep(2)
+    readFile = open('out.txt','r')
+    for line in readFile:
+        response = response + line +"\n"
+    readFile.close()
+    os.remove('out.txt')
+    return response
+
 if len(sys.argv) == 2:
     ip = sys.argv[1]
 else:
@@ -13,54 +28,58 @@ else:
     
 port = 4444
 server_address = (ip, port)
-protocols = ["udp", "tcp", "sctp", "run", "close"]
+protocols = ["udp", "tcp", "sctp"]
 protocol = ""
+
 
 while protocol not in protocols:
     protocol = input("Selecionar protocolo inicial (UDP, TCP, SCTP): ").lower()
 
 while True:
     if protocol == "udp":
-        print("udp")
+        print("Protocolo alterado para UDP.")
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.bind(server_address)
         data, client_address = sock.recvfrom(4096)
         command = data.decode('utf-8').lower()
         print(command)
-        if command in protocols:
+        if command == "run":
+            response = run_tcp_dump()
+            sock.sendto(response.encode('utf-8'), client_address)
+        elif command in protocols:
             protocol = command
     elif protocol == "tcp":
-        print("tcp")
+        print("Protocolo alterado para TCP.")
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.bind(server_address)
         sock.listen()
         connection, client_address = sock.accept()
         print("Conexão com " + client_address[0] + " estabelecida")
         command = connection.recv(4096).decode('utf-8')
-        print(command)
-        if command in protocols:
+        print("Comando recebido: " + command)
+        if command == "run":
+            response = run_tcp_dump()
+            sock.sendto(response.encode('utf-8'), client_address)
+        elif command in protocols:
             protocol = command
     elif protocol == "sctp":
-        print("sctp")
+        print("Protocolo alterado para SCTP.")
         sock = sctp.sctpsocket_tcp(socket.AF_INET)
         sock.bind(server_address)
         sock.listen(1)
         connection, client_address = sock.accept()
         print("Conexão com " + client_address[0] + " estabelecida")
         command = connection.recv(4096).decode('utf-8')
-        print(command)
-        if command in protocols:
+        print("Comando recebido: " + command)
+        if command == "run":
+            response = run_tcp_dump()
+            sock.sendto(response.encode('utf-8'), client_address)
+        elif command in protocols:
             protocol = command
-    elif protocol == "run":
-        comando = "lixo"
-        time.sleep(3)
-        sock.sendto(comando.encode('utf-8'), client_address)
-        data, client_address = sock.recvfrom(4096)
-    elif protocol == "close":
-        if protocol in ('tcp', 'sctp'):
-	        connection.close()
-        break
-    #sock.close()
+    if protocol in ('tcp', 'sctp'):
+	    connection.close()
+    sock.close()
+    time.sleep(3)
 
 
 	
